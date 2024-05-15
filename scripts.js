1aac2701cb0132a57ba5295efb32289940c505b6
Eric Tsai <erictsaiphoto@gmail.com>
2024-05-15 09:55:16 -0700

    set up header and footer, scripts

---
+++ scripts.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    // Allow for random header image
+    $('#footer').load('footer.html');
+    $('#header').load('header.html');
+    console.log('loading scripts');
+}
