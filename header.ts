8923fa0bd1f5a756fa3f32dca166a7c2f2764a0c
Kyle Cooley <kylekc@umich.edu>
2025-01-23 17:46:39 -0500

    Extract header code to separate files

--- header.ts
+++ header.ts
@@ -1,17 +1,59 @@
-export async function resizeHeaderLogo() {
+import $ from "jquery";
+
+window.addEventListener("load", () => {
+  $("#header").load("header.html", () => {
+    setUpHeader();
+  });
+});
+
+window.addEventListener("resize", async (): Promise<void> => {
+  await toggleHeaderIfNeeded();
+});
+
+function setUpHeader() {
+  const stylesheetLink = document.getElementById("header-stylesheet-link");
+  if (!stylesheetLink) {
+    return;
+  }
+  stylesheetLink.onload = async () => {
+    await resizeHeaderLogo();
+    initializeHamburgerMenuButton();
+    await toggleHeaderIfNeeded();
+  };
+}
+
+async function resizeHeaderLogo() {
   const headerLogo = document.getElementById("header-logo");
   const headerLink = document.querySelectorAll(".header-link")[1]; // the first one is the logo itself. second one is fine
   if (!headerLogo || !headerLink) {
     return;
   }
 
-  const headerLinkComputedStyle = getComputedStyle(headerLink);
-  headerLogo.style.height = `${headerLink.clientHeight - (parseFloat(headerLinkComputedStyle.paddingTop) + parseFloat(headerLinkComputedStyle.paddingBottom))}px`;
-
-  await new Promise((resolve) => setTimeout(resolve, 0));
+  headerLogo.style.height = `${headerLink.clientHeight - getTotalVerticalPadding(headerLink)}px`;
 }
 
-export function toggleHeaderIfNeeded() {
+function initializeHamburgerMenuButton() {
+  const hamburgerMenuButton = document.getElementById("hamburger-menu-button");
+  const hamburgerMenu = document.getElementById("hamburger-menu");
+  const hamburgerMenuContainer = document.getElementById(
+    "hamburger-menu-container",
+  );
+  if (!hamburgerMenuButton || !hamburgerMenu || !hamburgerMenuContainer) {
+    return;
+  }
+
+  hamburgerMenuButton.addEventListener("click", () => {
+    if (hamburgerMenu.classList.contains("hide")) {
+      hamburgerMenu.style.maxHeight = `${hamburgerMenuContainer.scrollHeight + getTotalVerticalPadding(hamburgerMenuContainer)}px`;
+      hamburgerMenu.classList.remove("hide");
+    } else {
+      hamburgerMenu.style.maxHeight = "0";
+      hamburgerMenu.classList.add("hide");
+    }
+  });
+}
+
+async function toggleHeaderIfNeeded() {
   const headerNav = document.querySelector(".header-nav");
   const hamburgerMenuButton = document.getElementById("hamburger-menu-button");
   const hamburgerMenu = document.getElementById("hamburger-menu");
@@ -46,11 +88,11 @@ export function toggleHeaderIfNeeded() {
   }
 
   if (!areLinksHidden && fullWidth > window.innerWidth) {
-    hamburgerMenuButton.style.display = "block";
+    hamburgerMenuButton.classList.remove("hide");
     navLinks.classList.add("hide");
     socialLinks.classList.add("hide");
   } else if (areLinksHidden && fullWidth <= window.innerWidth) {
-    hamburgerMenuButton.style.display = "none";
+    hamburgerMenuButton.classList.add("hide");
     navLinks.classList.remove("hide");
     socialLinks.classList.remove("hide");
     hamburgerMenu.style.maxHeight = "0";
@@ -58,20 +100,10 @@ export function toggleHeaderIfNeeded() {
   }
 }
 
-export function initializeHamburgerMenuButton() {
-  const hamburgerMenuButton = document.getElementById("hamburger-menu-button");
-  const hamburgerMenu = document.getElementById("hamburger-menu");
-  if (!hamburgerMenuButton || !hamburgerMenu) {
-    return;
-  }
-
-  hamburgerMenuButton.addEventListener("click", () => {
-    if (hamburgerMenu.classList.contains("hide")) {
-      hamburgerMenu.style.maxHeight = hamburgerMenu.scrollHeight + "px";
-      hamburgerMenu.classList.remove("hide");
-    } else {
-      hamburgerMenu.style.maxHeight = "0";
-      hamburgerMenu.classList.add("hide");
-    }
-  });
+function getTotalVerticalPadding(element: Element): number {
+  const computedStyle = getComputedStyle(element);
+  return (
+    (parseFloat(computedStyle.paddingTop) || 0) +
+    (parseFloat(computedStyle.paddingBottom) || 0)
+  );
 }
--- scripts.ts
+++ scripts.ts
@@ -3,24 +3,11 @@ import { initializeHideButton } from "./hide-button";
 import $ from "jquery";
 import "./shows";
 import "./initialize-swiper";
-import {
-  initializeHamburgerMenuButton,
-  resizeHeaderLogo,
-  toggleHeaderIfNeeded,
-} from "./header";
+import "./header";
 
-window.onload = (): void => {
+window.addEventListener("load", () => {
   $("#footer").load("footer.html");
-  $("#header").load("header.html", async () => {
-    await resizeHeaderLogo();
-    initializeHamburgerMenuButton();
-    toggleHeaderIfNeeded();
-  });
   $("#spotify-widget").load("spotify-widget.html", () => {
     initializeHideButton();
   });
-};
-
-window.onresize = (): void => {
-  toggleHeaderIfNeeded();
-};
+});
