cc2a706dad0fc920e21508bcafe4cdb25e865225
Kyle Cooley <kylekc@umich.edu>
2025-02-13 16:50:27 +0100

    Temporarily add debugging logs in `header.ts`

--- header/header.ts
+++ header/header.ts
@@ -1,38 +1,51 @@
 import $ from "jquery";
+import { logDebug } from "../scripts";
 
 window.addEventListener("load", () => {
   $("#header").load("header/header.html", () => {
+    logDebug("HERE 1");
     setUpHeader();
   });
 });
 
 window.addEventListener("resize", async (): Promise<void> => {
+  logDebug("HERE 2");
   await toggleHeaderIfNeeded();
 });
 
 function setUpHeader() {
+  logDebug("HERE 3");
   const stylesheetLink = document.getElementById("header-stylesheet-link");
   if (!stylesheetLink) {
     return;
   }
+  logDebug("HERE 4");
   stylesheetLink.onload = async () => {
+    logDebug("HERE 5");
     await resizeHeaderLogo();
     initializeHamburgerMenuButton();
     await toggleHeaderIfNeeded();
+    logDebug("HERE 6");
   };
 }
 
 async function resizeHeaderLogo() {
+  logDebug("HERE 7");
   const headerLogo = document.getElementById("header-logo");
   const headerLink = document.querySelectorAll(".header-link")[1]; // the first one is the logo itself. second one is fine
   if (!headerLogo || !headerLink) {
     return;
   }
 
+  logDebug("HERE 8");
+
   headerLogo.style.height = `${headerLink.clientHeight - getTotalVerticalPadding(headerLink)}px`;
+
+  logDebug("HERE 9");
 }
 
 function initializeHamburgerMenuButton() {
+  logDebug("HERE 10");
   const hamburgerMenuButton = document.getElementById("hamburger-menu-button");
   const hamburgerMenu = document.getElementById("hamburger-menu");
   const hamburgerMenuContainer = document.getElementById(
@@ -42,7 +55,10 @@ function initializeHamburgerMenuButton() {
     return;
   }
 
+  logDebug("HERE 11");
+
   hamburgerMenuButton.addEventListener("click", () => {
+    logDebug("HERE 12");
     if (hamburgerMenu.classList.contains("hide")) {
       hamburgerMenu.style.maxHeight = `${hamburgerMenuContainer.scrollHeight + getTotalVerticalPadding(hamburgerMenuContainer)}px`;
       hamburgerMenu.classList.remove("hide");
@@ -50,10 +66,14 @@ function initializeHamburgerMenuButton() {
       hamburgerMenu.style.maxHeight = "0";
       hamburgerMenu.classList.add("hide");
     }
+    logDebug("HERE 13");
   });
+
+  logDebug("HERE 14");
 }
 
 async function toggleHeaderIfNeeded() {
+  logDebug("HERE 15");
   const headerNav = document.querySelector(".header-nav");
   const hamburgerMenuButton = document.getElementById("hamburger-menu-button");
   const hamburgerMenu = document.getElementById("hamburger-menu");
@@ -69,6 +89,8 @@ async function toggleHeaderIfNeeded() {
   ) {
     return;
   }
+
+  logDebug("HERE 16");
 
   const areLinksHidden =
     navLinks.classList.contains("hide") ||
@@ -87,6 +109,8 @@ async function toggleHeaderIfNeeded() {
     socialLinks.classList.add("hide");
   }
 
+  logDebug("HERE 17");
+
   if (!areLinksHidden && fullWidth > window.innerWidth) {
     hamburgerMenuButton.classList.remove("hide");
     navLinks.classList.add("hide");
@@ -98,6 +122,8 @@ async function toggleHeaderIfNeeded() {
     hamburgerMenu.style.maxHeight = "0";
     hamburgerMenu.classList.add("hide");
   }
+
+  logDebug("HERE 18");
 }
 
 function getTotalVerticalPadding(element: Element): number {
